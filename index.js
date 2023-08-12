b5ef3b2cb9614db858367e137f03b46fa2eda70b
Jaswant Singh <rolvinss@gmail.com>
2023-08-13 00:31:41 +0530

    intial commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const axios = require('axios');
 const app = express()
 const PORT = 4000
 
@@ -7,8 +7,19 @@ app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
 })
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
+app.get('/:storyUrl',async (req, res) => {
+     const { storyUrl } = req.params;
+
+    try {
+      const response = await axios.get(`https://acadlog.in/web-stories/${storyUrl}`);
+      const html = response.data;
+
+      res.setHeader('Content-Type', 'text/html');
+      res.status(200).send(html);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('An error occurred while fetching the content');
+    }
 })
 
 app.get('/about', (req, res) => {
