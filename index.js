26ff37e7f53f9d6941dc76a45fe788fdeab4486e
Jaswant Singh <rolvinss@gmail.com>
2023-08-13 22:27:50 +0530

    wip

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
 const express = require('express');
 const axios = require('axios');
 const Redis = require('ioredis');
+const { JSDOM } = require('jsdom');
 const app = express();
 const PORT = 4000;
 
+
 // Configure Redis client
-const redis = new Redis("rediss://red-cffst6hgp3jjsea2p1c0:hsTt7ViwP8IrERyZaryFhRsIcV8x7xQ0@singapore-redis.render.com:6379");
+// const redis = new Redis("rediss://red-cffst6hgp3jjsea2p1c0:hsTt7ViwP8IrERyZaryFhRsIcV8x7xQ0@singapore-redis.render.com:6379");
+
+const redis = new Redis({
+  hostname: "localhost",
+  port: "6379"
+});
+
+function replaceOgUrl(html) {
+  const dom = new JSDOM(html);
+  
+  // Log the original HTML for debugging
+  console.log("Original HTML:", html);
+
+  const ogUrlMetaTag = dom.window.document.querySelector('meta[property="og:url"]');
+
+  if (ogUrlMetaTag) {
+    const content = ogUrlMetaTag.getAttribute('content');
+    if (content && content.includes('whitetigerhome.in')) {
+      ogUrlMetaTag.setAttribute('content', content.replace('whitetigerhome.in', 'acadlog.com'));
+    }
+  }
+
+  // Replace canonical link
+  const canonicalLink = dom.window.document.querySelector('link[rel="canonical"]');
+  
+  // Log the canonical link for debugging
+  console.log("Canonical Link Element:", canonicalLink);
+
+  if (canonicalLink) {
+    const href = canonicalLink.getAttribute('href');
+    if (href && href.includes('whitetigerhome.in')) {
+      canonicalLink.setAttribute('href', href.replace('whitetigerhome.in', 'acadlog.com'));
+    }
+  }
+
+  const updatedHtml = dom.serialize();
+  // Log the updated HTML for debugging
+  console.log("Updated HTML:", updatedHtml);
+
+  return updatedHtml;
+}
+
+
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT}`);
@@ -24,11 +68,14 @@ app.get('/:storyUrl', async (req, res) => {
       res.status(200).send(cachedData);
     } else {
       // Fetch the data if not in cache
-      const response = await axios.get(`https://acadlog.in/web-stories/${storyUrl}`);
-      const html = response.data;
+      const response = await axios.get(`https://whitetigerhome.in/web-stories/${storyUrl}`);
+      let html = response.data;
+
+      // Replace the og:url content
+      html = replaceOgUrl(html);
 
       // Store the data in Redis cache for future use
-      await redis.set(storyUrl, html, 'EX', 3600); // 3600 seconds expiration time
+      await redis.set(storyUrl, html, 'EX', 1); // 3600 seconds expiration time
 
       res.setHeader('Content-Type', 'text/html');
       res.status(200).send(html);
