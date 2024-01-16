6bc6b12eb89f90b81df74af584b0725b984f3369
Jaswant Singh <rolvinss@gmail.com>
2024-01-17 01:04:36 +0530

    updating url

--- index.js
+++ index.js
@@ -37,7 +37,6 @@ function replaceOgUrl(html) {
 
 app.get('/*', async (req, res) => {
   let path = req.params[0]
-  console.log(path)
   try {
       const response = await axios.get(`https://whitetigerhome.in/${path}`);
       let html = response.data;
